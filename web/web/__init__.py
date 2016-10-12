3d6e81191b5de24c00db131227fb31bd24eba139
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2016-10-12 22:41:21 +0100

    Add session and auth

--- web/web/__init__.py
+++ web/web/__init__.py
@@ -1,51 +1,6 @@
 from pyramid.config import Configurator
-from pyramid.interfaces import IAuthenticationPolicy
-from pyramid.security import Everyone, Authenticated
-from pyramid.authentication import AuthTktCookieHelper
-from pyramid.authorization import ACLAuthorizationPolicy
-from pyramid.settings import asbool
-
-def asint(val):
-	if val == 'None':
-		return None
-	return int(val)
-
-class GtmAuthenticate(object):
-	#Based on http://docs.pylonsproject.org/projects/pyramid_cookbook/en/latest/auth/custom.html
-	
-	def __init__(self, settings):
-		self.cookie = AuthTktCookieHelper(
-			settings.get('auth.secret'),
-			cookie_name=settings.get('auth.token') or 'auth_tkt',
-			secure=asbool(settings.get('auth.secure')),
-			timeout=asint(settings.get('auth.timeout')),
-			reissue_time=asint(settings.get('auth.reissue_time')),
-			max_age=asint(settings.get('auth.max_age')),
-		)
-
-	def remember(self, request, principal, **kw):
-		return self.cookie.remember(request, principal, **kw)
-
-	def forget(self, request):
-		return self.cookie.forget(request)
-
-	def unauthenticated_userid(self, request):
-		result = self.cookie.identify(request)
-		if result:
-			return result['userid']
-
-	def authenticated_userid(self, request):
-		if request.user:
-			return request.user.id
-
-	def effective_principals(self, request):
-		principals = [Everyone]
-		user = request.user
-		if user:
-			principals += [Authenticated, 'u:%s' % user.id]
-			principals.extend(('g:%s' % g.name for g in user.groups))
-		return principals
-
+from pyramid.session import SignedCookieSessionFactory
+import auth
 
 def main(global_config, **settings):
 	""" This function returns a Pyramid WSGI application.
@@ -53,12 +8,13 @@ def main(global_config, **settings):
 	config = Configurator(settings=settings)
 	config.include('pyramid_chameleon')
 
-	config.set_authentication_policy(GtmAuthenticate(settings=settings))
-	config.set_authorization_policy(ACLAuthorizationPolicy())
+	my_session_factory = SignedCookieSessionFactory(settings['auth.secret'])
+	config.set_session_factory(my_session_factory)
 
-	config.add_static_view('static', 'static', cache_max_age=3600)
+	config.add_static_view('static', 'static', cache_max_age=60)
 	config.add_route('home', '/')
 	config.add_route('login', '/login')
+	config.add_route('logout', '/logout')
 	config.scan()
 	return config.make_wsgi_app()
 
--- web/web/views.py
+++ web/web/views.py
@@ -1,11 +1,47 @@
 from pyramid.view import view_config
-
+from pyramid.httpexceptions import HTTPFound
+import gtm_wrapper
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
-    return {'project': 'web'}
+
+	username = None
+	if "username" in request.session:
+		username = request.session["username"]
+
+	return {'project': 'web', 'logged_in': username}
 
 @view_config(route_name='login', renderer='templates/login.pt')
 def login_view(request):
-    return {'project': 'web'}
+	login = ""
+	password = ""
+	message = None
+	login_url = request.route_url('login')
+	referrer = request.url
+	if referrer == login_url:
+		referrer = '/'  # never use login form itself as came_from
+	came_from = request.params.get('came_from', referrer)
+
+	if 'form.submitted' in request.params:
+		login = request.params['login']
+		password = request.params['password']
+		ok, username = gtm_wrapper.check_login(login, password)
+		if ok:
+			request.session["username"] = username
+			return HTTPFound(location=came_from)
+		message = 'Failed login'
+
+	return {'url': request.application_url + '/login',
+			'login': login,
+			'password': password,
+			'came_from': came_from,
+			'message': message,
+		}
+
+@view_config(route_name='logout')
+def logout_view(request):
+
+	request.session.invalidate()
+	url = request.route_url('home')
+	return HTTPFound(location=url)
 
